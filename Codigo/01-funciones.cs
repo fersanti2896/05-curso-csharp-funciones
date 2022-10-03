1fc256d28f53908ace56a12cd21e91cda1791162
Fernando Nicolás <fersanti2896@gmail.com>
2022-10-03 15:07:05 -0500

    Funciones en 01-funciones.cs

---
+++ Codigo/01-funciones.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones.Codigo {
+    public class _01_funciones {
+        public void Funciones() {
+            string obtenerNombre() {
+                return "Fernando Nicolás";
+            }
+
+            Console.WriteLine(obtenerNombre());
+
+            void fechaActual() {
+                var msg = $"La fecha de hoy es { DateTime.Today.ToString("g") }";
+                Console.WriteLine(msg);
+            }
+
+            fechaActual();
+
+            int diaActual() {
+                return DateTime.Today.Day;
+            }
+
+            Console.WriteLine($"Dia de la semana: { diaActual() }");
+        }
+    }
+}
