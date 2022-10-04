c7bf78c0a322a7d154b630a208efc588513075f3
Fernando Nicolás <fersanti2896@gmail.com>
2022-10-04 18:23:19 -0500

    Delegate en 12-delegados.cs

---
+++ Codigo/12-delegados.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones.Codigo {
+    public class _12_delegados {
+        public void delegados() {
+            Action action;
+            Func<int> func;
+            Predicate<int> predicado;
+
+            bool modificacion(string valor, out int resultado) {
+                return int.TryParse(valor, out resultado);
+            }
+
+            /* modificacionDelegado valor = modificacion;
+
+            int cantidad;
+
+            if (valor("12", out cantidad)) {
+                Console.WriteLine($"Valor: { cantidad }");
+            }
+
+            //delegate bool modificacionDelegado(string valor, out int resultado); */
+        }
+    }
+}
