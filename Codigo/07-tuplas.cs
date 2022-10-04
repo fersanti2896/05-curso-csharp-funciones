95faddc6016c70aba56d59b8c83da40b447ee615
Fernando Nicolás <fersanti2896@gmail.com>
2022-10-04 14:24:34 -0500

    Tuplas en 07-tuplas.cs

---
+++ Codigo/07-tuplas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones.Codigo {
+    public class _07_tuplas {
+        public void tuplas() {
+            var tupla = (99, 23, 12, "Fernando", true);
+            Console.WriteLine(tupla);
+
+            (int edad, bool bandera, string nombre) = (25, true, "Fer Santi");
+            Console.WriteLine($"Edad: { edad } | Nombre: { nombre } | Bandera: { bandera }");
+        }
+    }
+}
