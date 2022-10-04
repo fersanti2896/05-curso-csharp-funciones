ac76410b05387a9eca5a8e11fcbd9deaf9218f01
Fernando Nicolás <fersanti2896@gmail.com>
2022-10-04 15:08:23 -0500

    Funciones Locales y Lambda en 08-funciones-locales.cs

---
+++ Codigo/08-funciones-locales.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones.Codigo {
+    internal class _08_funciones_locales {
+        public void funcionesLocales() {
+            void imprimeResultado(int cantidad) {
+                Console.WriteLine($"El valor es: { cantidad }");
+            }
+
+            void sumarValores(int a, int b) {
+                int suma = a + b;
+                imprimeResultado(suma);
+            }
+
+            sumarValores(10, 2);
+
+            int multiplicar(int a, int b) => a * b;
+        }
+    }
+}
