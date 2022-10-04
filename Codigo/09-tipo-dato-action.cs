a2be1ead7133198bde7c95f7b91b9c1a211d06b9
Fernando Nicolás <fersanti2896@gmail.com>
2022-10-04 17:28:05 -0500

    Tipo de Dato Action en 09-tipo-dato-action.cs

---
+++ Codigo/09-tipo-dato-action.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones.Codigo {
+    public class _09_tipo_dato_action {
+        public void Action() {
+            Action imprimeMensaje;
+
+            void imprimirFechaAct() {
+                Console.WriteLine(DateTime.Now.ToString());
+            }
+
+            void imprimeNombre() {
+                Console.WriteLine("Fernando Nicolás");
+            }
+
+            imprimeMensaje = imprimeNombre;
+            imprimeMensaje();
+
+            imprimeMensaje = imprimirFechaAct;
+            imprimeMensaje();
+
+            Console.WriteLine();
+
+            void procesar(Action action) {
+                Console.WriteLine("Antes de ejecutar el action");
+                action();
+                Console.WriteLine("Después de ejecutar el action");
+            }
+
+            procesar(imprimirFechaAct);
+            Console.WriteLine();
+            procesar(imprimeNombre);
+
+            Action<string, int> imprimeNVeces;
+
+            void imprimeMsg(string msg, int num) {
+                for (int i = 0; i < num; i++) {
+                    Console.WriteLine(msg);
+                }
+            }
+
+            imprimeMsg("Fernando", 10);
+            imprimeNVeces = imprimeMsg;
+        }
+    }
+}
