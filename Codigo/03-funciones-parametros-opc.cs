a5839773d79b408660f6555d5e22aaa8837b8693
Fernando Nicolás <fersanti2896@gmail.com>
2022-10-04 01:02:05 -0500

    Parametros opcionales en 03-funciones-parametros-opc.cs

---
+++ Codigo/03-funciones-parametros-opc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones.Codigo { 
+    public class _03_funciones_parametros_opc {
+        public void funcionesParametrosOpcionales() {
+            void imprimeConsola(string msg, bool mayus = true) {
+                if (mayus) {
+                    msg = msg.ToUpper();
+                }
+
+                Console.WriteLine(msg);
+            }
+
+            imprimeConsola("Fernando quiere a Mar");
+        }
+    }
+}
