8c55d008a9b7bc1781742d4c7f9ef472bcf23158
Fernando Nicolás <fersanti2896@gmail.com>
2022-10-04 18:13:54 -0500

    Predicate en 11-predicados.cs

---
+++ Codigo/11-predicados.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones.Codigo {
+    public class _11_predicados {
+        public void predicados() {
+            bool esPar(int num) {
+                return num % 2 == 0;
+            }
+
+            Predicate<int> predicado = esPar;
+
+            var valor = 5;
+            Console.WriteLine($"¿Es { valor } par? { predicado(valor) }");
+
+            var valor2 = 6;
+            Console.WriteLine($"¿Es { valor2 } par? { predicado(valor2) }");
+        }
+    }
+}
